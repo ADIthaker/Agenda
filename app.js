315057539ca4ea138313366d9056d2b9a6ea2876
ADIthaker <56800392+ADIthaker@users.noreply.github.com>
2021-04-08 20:42:58 +0530

    deployment changes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ dbConnection.db();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({origin:'http://localhost:3000'}));
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 app.use(scheduleRoutes);
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
