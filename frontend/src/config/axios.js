6b8119fe8aa868e8d9121d4db0689a8b74c5db6a
ADIthaker <56800392+ADIthaker@users.noreply.github.com>
2021-04-08 21:54:06 +0530

    done

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Req = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://secret-earth-78161.herokuapp.com",
   });
   
 export default Req;
